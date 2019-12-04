6171957d5158d0a9333ebd3e76879b1fd197e580
Shane Utt <shaneutt@users.noreply.github.com>
2019-12-04 14:52:29 -0500

    Add Jenkins (#6)
    * Add initial version of jenkins.
    
    * Fix indentations
    
    * Add metadata for Jenkins.
    
    * Fix addon revision for Jenkins
    
    * Change minsupported for Jenkins
    
    Technically there's no reason for us to claim support for a version
    that we don't actually test against at this time, later we can easily choose
    to expand support, but the opposite is not as easily the case.
    
    * Add test filters for Jenkins
    
    * Dont hardcode a namespace, should deploy to any
    
    * Add jenkins override values for CI

--- test/addons_test.go
+++ test/addons_test.go
@@ -5,9 +5,10 @@ import (
 	"testing"
 
 	"github.com/blang/semver"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/mesosphere/kubeaddons/api/v1beta1"
 	"github.com/mesosphere/kubeaddons/hack/temp"
+	"github.com/mesosphere/kubeaddons/pkg/api/v1beta1"
 	"github.com/mesosphere/kubeaddons/pkg/test"
 	"github.com/mesosphere/kubeaddons/pkg/test/cluster/kind"
 )
@@ -29,12 +30,12 @@ func TestAddons(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// Kafka needs special attention right now to ensure it can find its dependency ZK
-	if revisions, ok := addons["kafka"]; ok {
-		for _, revision := range revisions {
-			zkuri := fmt.Sprintf("ZOOKEEPER_URI: zookeeper-cs.%s.svc", addons["zookeeper"][0].GetNamespace())
-			revision.GetAddonSpec().KudoReference.Parameters = &zkuri
-		}
+	// TODO: Kafka needs special attention right now to ensure it can find its dependency ZK
+	kafkaFilters(addons)
+
+	// TODO: for speed, prometheus options and requirements are disabled for Jenkins for now
+	if err := jenkinsFilters(addons); err != nil {
+		t.Fatal(err)
 	}
 
 	testAddons := []v1beta1.AddonInterface{}
@@ -54,4 +55,49 @@ func TestAddons(t *testing.T) {
 	th.Validate()
 	th.Deploy()
 
+}
+
+func kafkaFilters(addons map[string][]v1beta1.AddonInterface) {
+	if revisions, ok := addons["kafka"]; ok {
+		for _, revision := range revisions {
+			zkuri := fmt.Sprintf("ZOOKEEPER_URI: zookeeper-cs.%s.svc", addons["zookeeper"][0].GetNamespace())
+			revision.GetAddonSpec().KudoReference.Parameters = &zkuri
+		}
+	}
+}
+
+// TODO - due to issues with how CRDs are handled by the helm chart for Jenkins
+// we're going to use this to disable options that would utilize CRDs that wont
+// actually be deployed by the chart. This is something that will need to be improved upstream.
+var jenkinsOverrides = `---
+master:
+  useSecurity: false
+  installPlugins:
+    - kubernetes:1.18.2
+    - workflow-job:2.33
+    - workflow-aggregator:2.6
+    - credentials-binding:1.19
+    - git:3.11.0
+  csrf:
+    defaultCrumbIssuer:
+      enabled: false
+      proxyCompatability: false
+  serviceType: "ClusterIP"
+  jenkinsUriPrefix: "/jenkins"
+  path: /jenkins
+  ingress:
+    enabled: false
+  prometheus:
+    enabled: false
+`
+
+func jenkinsFilters(addons map[string][]v1beta1.AddonInterface) error {
+	if revisions, ok := addons["jenkins"]; ok {
+		for _, revision := range revisions {
+			// TODO: for now we're going to remove deps for speed, jenkins can deploy usably without traefik.
+			revision.GetAddonSpec().Requires = make([]v1.LabelSelector, 0)
+			revision.GetAddonSpec().ChartReference.Values = &jenkinsOverrides
+		}
+	}
+	return nil
 }
